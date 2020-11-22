bc944ee2ee5f3bba8edc3c9ddbf234a89e429bee
Elram Espra <enespra@gmail.com>
2020-11-23 02:21:51 +0800

    [Elram] Fixed linksk

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -1,3 +1,14 @@
 $('.search-bar .icon').on('click', function() {
   $(this).parent().toggleClass('active');
 });
+
+var winHeight = $(".calendar").height();
+var height = ( winHeight * 16.6666 ) / 100;
+var lineHeight = height + "px";
+
+$("#fullDiv li").css("line-height", lineHeight);
+$("#fullDiv li").css("height", height);
+
+$(".search-bar").change(function(){
+  window.location.href = "/search";
+});
